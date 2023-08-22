7224164cf2f5b166e48b89c5c0c0b1ca3f0e7419
thdevfull <thallescrj@gmail.com>
2023-08-22 11:44:36 -0300

    Exemplo de Casting

--- tiposVariaveis/tiposVariaveis/src/TiposVariaveis.java
+++ tiposVariaveis/tiposVariaveis/src/TiposVariaveis.java
@@ -2,5 +2,17 @@ public class TiposVariaveis {
 
     public static void main(String[] args) {
         System.out.println("Aula top");
+
+
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
     }
 }
+
+short numeroCurto = 1;
+int numeroNormal = numeroCurto;
+short numeroCurto2 = (short) numeroNormal;
